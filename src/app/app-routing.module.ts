5f7f89996dffd3d8c23333bfc30f7423b77f0a6d
pgabriel <pgabriel.26@gmail.com>
2023-10-17 13:18:08 -0300

    adding toolbar menu, links to other components and fixing routing of them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     component: FilmesComponent
   },
   {
-    path: 'nave',
+    path: 'naves',
     component: NaveComponent
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatSlideToggleModule  } from '@angular/material/slide-toggle';
+import { MatToolbarModule  } from '@angular/material/toolbar';
+import { MatIconModule  } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,7 +25,13 @@ import { NaveComponent } from './components/nave/nave.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
