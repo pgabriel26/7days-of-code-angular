9cc161fbaf5b908253d2398d0b928c6e2133545d
pgabriel <pgabriel.26@gmail.com>
2023-10-24 14:03:15 -0300

    implementing nave component

--- src/app/components/pages/nave/nave.component.ts
+++ src/app/components/pages/nave/nave.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Starship, StarshipResults } from 'src/app/models/interfaces';
+import { ApiService } from 'src/app/shared/api.service';
 
 @Component({
   selector: 'app-nave',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./nave.component.css']
 })
 export class NaveComponent {
+
+  retorno!: StarshipResults;
+  naves: Starship[] = [];
+
+  displayedColumns = ['fabricante','nome','modelo',
+                      'tamanho','capacidade','preco',
+                      'equipe','classe','ranking',
+                      'pilotos','filme','criada'];
+
+  constructor( private service: ApiService) {}
+
+  ngOnInit():  void {
+    this.getStarships('');
+  }
+
+  callSearch(event: any, form: NgForm){
+    const busca = event.target.value;
+    console.log('realizando busca = ', busca);
+    if (form.valid) {
+      this.naves = [];
+      this.getStarships(busca)
+    }
+  }
+
+  getStarships(search: string) {
+    console.log('buscando naves...');
+    
+    this.service.getNaves(search).subscribe( 
+      response => {
+        this.retorno = response;
+        this.naves = this.retorno.results;
+        console.log('busca concluida');
+        console.log(this.naves); 
+    });
+  }
+
+  formatData(data: string) {
+    const dataObj = new Date(data);
+    return dataObj.toLocaleDateString('pt-BR');
+  }
 
 }
